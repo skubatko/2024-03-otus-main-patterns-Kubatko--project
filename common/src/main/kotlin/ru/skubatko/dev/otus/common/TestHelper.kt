731c03cd5d476808452e8455c9c3c93e39e79da2
Sergey Kubatko <skubatko@gmail.com>
2024-07-27 16:24:09 +0300

    common tests added

---
+++ common/src/main/kotlin/ru/skubatko/dev/otus/common/TestHelper.kt
@@ -0,0 +1,14 @@
+package ru.skubatko.dev.otus.common
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.matching.RequestPatternBuilder
+import io.mockk.MockKVerificationScope
+import io.mockk.verify
+
+fun verifyOnce(action: MockKVerificationScope.() -> Unit) = verify(exactly = 1, verifyBlock = action)
+
+fun verifyNotCalled(action: MockKVerificationScope.() -> Unit) = verify(exactly = 0, verifyBlock = action)
+
+fun WireMockServer.verifyOnce(requestPatternBuilder: RequestPatternBuilder) = this.verify(1, requestPatternBuilder)
+
+fun WireMockServer.verifyNotCalled(requestPatternBuilder: RequestPatternBuilder) = this.verify(0, requestPatternBuilder)
