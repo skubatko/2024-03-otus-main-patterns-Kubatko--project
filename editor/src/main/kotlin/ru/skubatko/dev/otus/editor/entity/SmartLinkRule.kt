5d8d2b4c5494311d36ed52d6cc89fbcb333109e1
Sergey Kubatko <skubatko@gmail.com>
2024-08-07 10:10:30 +0300

    editor entity updated

--- editor/src/main/kotlin/ru/skubatko/dev/otus/editor/entity/SmartLinkRule.kt
+++ editor/src/main/kotlin/ru/skubatko/dev/otus/editor/entity/SmartLinkRule.kt
@@ -1,12 +1,13 @@
 package ru.skubatko.dev.otus.editor.entity
 
+import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import ru.sokomishalov.commons.core.consts.EMPTY
 
 @Document("smart_link_rules")
 data class SmartLinkRule(
-    @Id val id: String = EMPTY,
+    @Id val id: String = ObjectId().toHexString(),
     val name: String = EMPTY,
     val rule: String = EMPTY,
 )
--- editor/src/main/kotlin/ru/skubatko/dev/otus/editor/mapper/SmartLinkRuleMappers.kt
+++ editor/src/main/kotlin/ru/skubatko/dev/otus/editor/mapper/SmartLinkRuleMappers.kt
@@ -3,7 +3,6 @@ package ru.skubatko.dev.otus.editor.mapper
 import ru.skubatko.dev.otus.editor.entity.SmartLinkRule
 import ru.skubatko.dev.otus.editor.model.SmartLinkRuleDto
 import ru.skubatko.dev.otus.editor.model.SmartLinkRuleUpdateDto
-import java.util.*
 
 fun SmartLinkRule.toDto(): SmartLinkRuleDto =
     SmartLinkRuleDto(
@@ -14,7 +13,6 @@ fun SmartLinkRule.toDto(): SmartLinkRuleDto =
 
 fun SmartLinkRuleUpdateDto.toEntity(): SmartLinkRule =
     SmartLinkRule(
-        id = UUID.randomUUID().toString(),
         name = this.name,
         rule = this.rule
     )
